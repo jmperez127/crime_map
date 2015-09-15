e6ad34fd9adaf6f6f50edea16adda6828c95a543
theil <theil_27@hotmail.com>
2015-09-15 03:35:18 -0430

    Add latitude and longitud properties to client

--- client/app/models/crime.js
+++ client/app/models/crime.js
@@ -6,6 +6,12 @@ export default DS.Model.extend({
   latLong: DS.attr(), //array
   // TODO author and type relationships
   author: DS.attr(),
+  latitude: function(){
+    return this.get("latLong")[0];
+  }.property(),
+  longitude: function(){
+    return this.get("latLong")[1];
+  }.property(),
   type: DS.attr(),
   source: DS.attr('string'),
   confirmations: DS.attr('number'),
